dcf8040f16d010ee739ae7654bdbc82c0ca85596
Morteza Fard Saffari <mortezafardsaffari@yahoo.com>
2023-05-05 22:21:03 +0330

    Register New user

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,7 +6,11 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  let usersWithSameUsername = users.filter((user)=> {
+    return user.username === username
+  });
+
+  return !(usersWithSameUsername.length > 0);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
