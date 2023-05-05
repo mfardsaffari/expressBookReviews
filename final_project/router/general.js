2eed8f9e47ca44a9279cc450544b42a3f1810b0a
Morteza Fard Saffari <mortezafardsaffari@yahoo.com>
2023-05-05 22:14:22 +0330

    Get book Review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -58,8 +58,16 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const searchedISBN = parseInt(req.params.isbn);
+  if(books[searchedISBN])
+  {
+    if(JSON.stringify(books[searchedISBN].reviews) !== '{}')
+      return res.send(JSON.stringify(books[searchedISBN].reviews,null,4));
+    else
+      return res.status(300).json({message: "No reviews have been submitted for this book!"});
+  }
+  else
+    return res.status(300).json({message: "Book not found!"});
 });
 
 module.exports.general = public_users;
