43fd45ec89a31be2fa7d86b5320aa70b34be070c
Morteza Fard Saffari <mortezafardsaffari@yahoo.com>
2023-05-05 22:08:37 +0330

    Get the books based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,11 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const searchedISBN = parseInt(req.params.isbn);
+  if(books[searchedISBN])
+    return res.send(JSON.stringify(books[searchedISBN],null,4));
+  else
+    return res.status(300).json({message: "Book not found!"});
  });
 
 // Get book details based on author
